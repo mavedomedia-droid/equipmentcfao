b852c939504a171658e003f533287fb6bfcd85e8
mavedomedia-droid <mavedomedia@gmail.com>
2026-08-20 19:11:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GAS_URL = "https://script.google.com/macros/s/YOUR_ACTUAL_ENDPOINT/exec";
+window.GAS_URL = "https://script.google.com/macros/s/AKfycbyLn3znQc6YodSlpebObssVZYc7R_kZIqbRD6_b7LzG7_8Md17PBoF2ZrNBgM6jHpry/exec";
